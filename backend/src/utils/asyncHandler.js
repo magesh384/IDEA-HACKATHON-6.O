df05bfef225b2a15f734cc7f56360990c952ca6e
MAGESHWARAN.K <mageshd384@gmail.com>
2026-08-01 13:09:03 +0530

    Delete backend directory

--- backend/src/utils/asyncHandler.js
+++
@@ -1,4 +0,0 @@
-// Wraps async route handlers so rejected promises are forwarded to Express's error middleware
-module.exports = function asyncHandler(fn) {
-  return (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
-};
