975546031a2709f9ba8db874f4edfffd7dcb9e9b
MAGESHWARAN.K <mageshd384@gmail.com>
2026-08-01 13:09:18 +0530

    Delete frontend directory

--- frontend/src/charts/setup.js
+++
@@ -1,8 +0,0 @@
-import {
-  Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, BarElement,
-  ArcElement, Title, Tooltip, Legend, Filler,
-} from 'chart.js'
-
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarElement, ArcElement, Title, Tooltip, Legend, Filler)
-
-export const CHART_COLORS = ['#6366f1', '#22c55e', '#f59e0b', '#ef4444', '#06b6d4', '#a855f7', '#ec4899']
